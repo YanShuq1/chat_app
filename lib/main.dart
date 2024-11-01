d7636a057565fe10fa76848f50ed5a184d9f337c
niuniu0101 <niuniu0101@noreply.gitcode.com>
2024-11-01 22:28:56 +0800

    feat:Refactoring the shot for data persistence

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:chat_app/model/shotModel.dart';
 import 'package:chat_app/pages/home/home.dart';
 import 'package:chat_app/provider/contact_provider.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(    
+Future<void> main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(ShotModelAdapter()); // 注册适配器
+  runApp(
     ChangeNotifierProvider(
       //监听覆盖
       create: (_) => ContactProvider(),
       child: const MyApp(),
-    ),);
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/model/shotModel.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shotModel.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ShotModelAdapter extends TypeAdapter<ShotModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  ShotModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ShotModel(
+      imagePath: fields[0] as String,
+      avatarPath: fields[1] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ShotModel obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.imagePath)
+      ..writeByte(1)
+      ..write(obj.avatarPath);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ShotModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
